fc033de268df874cc2c902b4b5d520f01541a0f0
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2018-12-27 23:20:40 +1100

    Debug

--- build&run.sh
+++ build&run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mvn clean package ./Movie
-mvn clean package ./MovieManager
+mvn clean package ~/Movie_recommend/Movie
+mvn clean package ~/Movie_recommend/MovieManager
 rm -f /usr/tomcat8/webapps/Movie.war
 rm -f /usr/tomcat8/webapps/MovieManager.war
-cp ./Movie/target/Movie.war /usr/tomcat8/webapps
-cp ./Movie/target/MovieManager.war /usr/tomcat8/webapps
+cp ~/Movie_recommend/Movie/target/Movie.war /usr/tomcat8/webapps
+cp ~/Movie_recommend/Movie/target/MovieManager.war /usr/tomcat8/webapps
 /usr/tomcat8/bin/catalina.sh run
